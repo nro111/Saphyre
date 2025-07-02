634080ee6825212b9edb7e41b62edcad48407136
nro111 <7928238+nro111@users.noreply.github.com>
2025-07-01 21:22:00 -0400

    Updating UI project by removing unnecessary pages/components, adding UserForm, adding UserGrid, and css modifications for dark mode

---
+++ src/Tests/Modules.Tests/Users/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.Tests.Users.Repositories
+{
+    public class UserRepositoryTests
+    {
+        [Fact()]
+        public void UserRepositoryTest()
+        {
+            Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CreateTest()
+        {
+            Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DeleteTest()
+        {
+            Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetTest()
+        {
+            Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetAllTest()
+        {
+            Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void UpdateTest()
+        {
+            Assert.Fail("This test needs an implementation");
+        }
+    }
+}
