1243a1f25a87b3646af29c98e2540f4117b5c295
nro111 <7928238+nro111@users.noreply.github.com>
2025-07-03 19:12:30 -0400

    Updating the api port in the blazor application and adding to the API.http file

--- src/Web/UI/Program.cs
+++ src/Web/UI/Program.cs
@@ -6,12 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient("Api", client =>
+builder.Services.AddHttpClient<IApiClient, ApiClient>("Api", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:5001/api/"); // adjust port
+    client.BaseAddress = new Uri("https://localhost:7052");
 });
-
-builder.Services.AddScoped<IApiClient, ApiClient>();
 
 var app = builder.Build();
 
